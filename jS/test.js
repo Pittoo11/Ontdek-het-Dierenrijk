759f658e4dd92875a4789156dc214773ae5d49d2
Annie <a.janice.chen@gmail.com>
2021-05-30 14:23:17 +0200

    tried putting title on screen but didnt work

--- jS/test.js
+++ jS/test.js
@@ -1,5 +1,17 @@
 window.addEventListener('load', init);
 
-function init() {
+let game = document.getElementById("game");
 
+function init() {
+    createText();
+}
+
+function createText(){
+    let gameCard = document.createElement('div');
+    gameCard.classList.add('gameCard');
+    game.appendChild(gameCard);
+
+    let title = document.createElement('h1');
+    title.innerText = "Titelscherm";
+    gameCard.appendChild(title);
 }
