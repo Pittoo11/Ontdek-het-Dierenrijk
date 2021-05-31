8fe6ddd7e0de18c8569ac8710d5112c5615e4d3b
jelmerrosdorf <jelmerr99@gmail.com>
2021-05-31 11:50:34 +0200

    Added font, made a beginning on the first word for the game

--- game.php
+++ game.php
@@ -1,1 +1,30 @@
 <?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="css/stylesheet.css"/>
+</head>
+
+
+<header>
+    <h1>HALLO DIT IS EEN TEST</h1>
+</header>
+
+
+<body>
+    <img src="images/vis.png" width="750" height="750">
+    <section class="word">
+        <box>v</box> <box>i</box> <box>s</box>
+    </section>
+</body>
+
+
+<footer>
+
+</footer>
