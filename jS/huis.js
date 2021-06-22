82884b5e9e4bf074784e64220c91e621d80bf8e6
maxho <0982541@hr.nl>
2021-06-22 17:35:51 +0200

    Added words boom and aap with tts and changed their images

---
+++ jS/huis.js
@@ -0,0 +1,52 @@
+function button() {
+    const btn = document.createElement("button");
+    btn.innerHTML = "volgende";
+    btn.type = "submit";
+    btn.addEventListener("click", function () {
+        location.href = "collage3.html"
+    })
+    document.body.appendChild(btn)
+}
+
+
+
+
+function removeBtn(number){
+    const elem = document.getElementById('btn'+ number);
+    elem.parentNode.removeChild(elem);
+}
+
+
+
+
+document.getElementById("btn4").addEventListener("click", function () {
+    document.getElementById("B1").innerHTML = "ui";
+    removeBtn(4)
+    removeBtn(1)
+    removeBtn(2)
+    removeBtn(3)
+    button()
+})
+
+
+
+document.getElementById('btn1').addEventListener("click", function () {
+        alert("Niet de goede, probeer het nog een keer")
+        removeBtn(1)
+
+    }
+)
+
+document.getElementById('btn2').addEventListener("click", function () {
+        alert("Niet de goede, probeer het nog een keer")
+        removeBtn(2)
+
+    }
+)
+document.getElementById('btn3').addEventListener("click", function () {
+        alert("Niet de goede, probeer het nog een keer")
+        removeBtn(3)
+
+    }
+)
+
